f76c117f75bb06356d26c3dcd71440b54cd85ee8
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-11-07 18:37:46 +0200

    This problem is the Daily LeetCoding Challenge for November, Day 7.

---
+++ Maximum69Number/Maximum69Number/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maximum69Number
+{
+    class Program
+    {
+        public int[] DigitToArray(int num)
+        {
+            List<int> numList = new List<int>();
+
+            while (num != 0)
+            {
+                numList.Add(num % 10);
+
+                num /= 10;
+            }
+
+            int[] arr = numList.ToArray();
+            Array.Reverse(arr);
+            return arr;
+        }
+
+        public int maximum69Number(int num)
+        {
+            int[] arr = DigitToArray(num);
+            string str = null;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 6)
+                {
+                    arr[i] = 9;
+                    break;
+                }
+            }
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += arr[i];
+            }
+
+            return Convert.ToInt32(str);
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            int num = 9669;
+
+            Console.WriteLine(program.maximum69Number(num));
+        }
+    }
+}
