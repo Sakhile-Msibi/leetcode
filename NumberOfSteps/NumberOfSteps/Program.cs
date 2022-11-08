9adf6ce3ca8f8a34163ace0bcefd584ad5ab09a1
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-11-08 05:40:35 +0200

    This problem is the Daily LeetCoding Challenge for May, Day 27

---
+++ NumberOfSteps/NumberOfSteps/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NumberOfSteps
+{
+    class Program
+    {
+        public int numberOfSteps(int num)
+        {
+            int steps = 0;
+
+            if (num == 0)
+                return steps;
+
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    steps++;
+                }
+
+                if (num % 2 != 0)
+                {
+                    num -= 1;
+                    steps++;
+                }
+            }
+
+            return steps;
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            int num = 123;
+
+            Console.WriteLine(program.numberOfSteps(num));
+        }
+    }
+}
