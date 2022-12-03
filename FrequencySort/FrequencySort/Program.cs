c5d482727a52131851371fcdf0dd60f4d41f4e34
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-12-03 19:29:34 +0200

    451. Sort Characters By Frequency

--- FrequencySort/FrequencySort/Program.cs
+++ FrequencySort/FrequencySort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,40 +27,73 @@ namespace FrequencySort
                     {'H', 0}, {'I', 0}, {'J', 0}, {'K', 0}, {'L', 0}, {'M', 0}, {'N', 0},
                     {'O', 0}, {'P', 0}, {'Q', 0}, {'R', 0}, {'S', 0}, {'T', 0}, {'U', 0},
                     {'V', 0}, {'W', 0}, {'X', 0}, {'Y', 0}, {'Z', 0},
+
+                    {'0', 0}, {'1', 0}, {'2', 0}, {'3', 0}, {'4', 0}, {'5', 0}, {'6', 0},
+                    {'7', 0}, {'8', 0}, {'9', 0}
                 };
 
                 StringBuilder stringS = new StringBuilder(s);
-                StringBuilder stringSol = new StringBuilder();
 
                 for (int i = 0; i < stringS.Length; i++)
                 {
-                    char ch = s[i];
-                    ch = 'A';
+                    char ch = stringS[i];
                     for (int j = i; j < stringS.Length; j++)
                     {
                         if (stringS[j] >= 'a' && stringS[j] <= 'z')
                         {
-                            if (stringS[i] == chars.ElementAt(stringS[j] - 97).Key)
+                            if (ch == stringS[j])
                             {
                                 chars[chars.ElementAt(stringS[j] - 97).Key]++;
                                 stringS.Remove(j, 1);
                                 j--;
                             }
                         }
-                        else
+                        else if (stringS[j] >= 'A' && stringS[j] <= 'Z')
                         {
-                            if (stringS[i] == chars.ElementAt(stringS[j] - 65).Key)
+                            if (ch == stringS[j])
                             {
-                                chars[chars.ElementAt(stringS[j] - 65).Key]++;
+                                chars[chars.ElementAt(stringS[j] - 39).Key]++;
                                 stringS.Remove(j, 1);
                                 j--;
                             }
                         }
-                        
+                        else
+                        {
+                            if (ch == stringS[j])
+                            {
+                                chars[chars.ElementAt(stringS[j] + 4).Key]++;
+                                stringS.Remove(j, 1);
+                                j--;
+                            }
+                        }
+
                     }
                     i--;
 
                 }
+
+                List<string> list = new List<string>();
+                foreach (var ch in chars)
+                {
+                    if (ch.Value > 0)
+                    {
+                        stringS = new StringBuilder();
+                        for (int i = 0; i < ch.Value; i++)
+                        {
+                            stringS.Append(ch.Key);
+                        }
+                        list.Add(stringS.ToString());
+                    }
+                }
+
+                list.Sort((s1, s2) => s1.Length - s2.Length);
+                stringS = new StringBuilder();
+
+                for (int i = list.Count - 1; i >= 0 ; i--)
+                {
+                    stringS.Append(list[i]);
+                }
+
 
                 return stringS.ToString();
             }
@@ -68,9 +102,13 @@ namespace FrequencySort
         {
             Program program = new Program();
 
-            string s = "aAabBcC";
+            string s = "1z33zb4bb55555bBccC";
 
-            Console.WriteLine(program.SortByFrequency(s));
+            //Console.WriteLine(program.SortByFrequency(s));
+            //Console.WriteLine(program.SortByFrequency("tree"));
+            //Console.WriteLine(program.SortByFrequency("cccaaa"));
+            //Console.WriteLine(program.SortByFrequency("Aabb"));
+            Console.WriteLine(program.SortByFrequency("2a554442f544asfasssffffasss"));
             Console.ReadKey();
         }
     }
