144f86e31633fca4d24fa1a49efc1900147dc684
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-12-14 19:18:21 +0200

    Inserts a new node after the given prev_node.

---
+++ NewMiddleNode/NewMiddleNode/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace NewMiddleNode
+{
+    class LinkedList
+    {
+        Node head;
+
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+        public void InsertAfter(Node prev_node, int new_data)
+        {
+            if (prev_node == null)
+                return;
+
+            Node new_node = new Node(new_data);
+
+            new_node.next = prev_node.next;
+            prev_node.next = new_node;
+        }
+        public void DisplayLinkedList()
+        {
+            Node n = head;
+
+            while (n != null)
+            {
+                Console.Write(n.data + " ");
+                n = n.next;
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.head = new Node(1);
+            Node second = new Node(2);
+            Node third = new Node(4);
+
+            linkedList.head.next = second;
+            second.next = third;
+
+            linkedList.DisplayLinkedList();
+
+            linkedList.InsertAfter(second, 3);
+            linkedList.DisplayLinkedList();
+            Console.ReadKey();
+        }
+    }
+}
