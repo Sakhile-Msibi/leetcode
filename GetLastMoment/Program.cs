ecf267fa65c65dd3c60db73ad1f19ddb1679155a
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2023-11-04 07:20:30 +0200

    1503. Last Moment Before All Ants Fall Out of a Plank

---
+++ GetLastMoment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GetLastMoment
+{
+    class Program
+    {
+        public int GetLMoment(int n, int[] left, int[] right)
+        {
+            if (left.Length == 0 && right.Length == 0)
+                return 0;
+
+            Array.Sort(left);
+            Array.Sort(right);
+
+            if (left.Length == 0)
+            {
+                return n - right[0];
+            }
+            else if (right.Length == 0)
+            {
+                return left[left.Length - 1];
+            }
+            else if (left.Length == 1 && right.Length == 1)
+            {
+                return (left[left.Length - 1] > right[right.Length - 1]) ? left[left.Length - 1] : right[right.Length - 1];
+            }
+            else
+            {
+                return n;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            int n = 20;
+            int[] left = { 4, 7, 15 };
+            int[] right = { 9, 3, 13, 10 }; 
+
+            Console.WriteLine(program.GetLMoment(n, left, right));
+        }
+    }
+}
