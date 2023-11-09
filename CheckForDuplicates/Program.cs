33f7ab49200e416c3de6bc2814e32e95ad0d9011
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2023-11-09 03:46:56 +0200

    Contains Duplicate

---
+++ CheckForDuplicates/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CheckForDuplicates
+{
+    internal class Program
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> hashSet = new HashSet<int>();
+
+            foreach (int key in nums)
+            {
+                if (hashSet.Contains(key))
+                {
+                    return true;
+                }
+                hashSet.Add(key);
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            int[] nums = { 1, 2, 3, 1 };
+            int[] nums1 = { 1, 2, 3, 4 };
+            int[] nums2 = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+            Console.WriteLine(program.ContainsDuplicate(nums));
+            Console.WriteLine(program.ContainsDuplicate(nums1));
+            Console.WriteLine(program.ContainsDuplicate(nums2));
+
+            Console.ReadKey();
+        }
+    }
+}
