479ea27ec0fd1a88e63f4695d615f7ab2c7c05d9
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2023-11-22 04:56:53 +0200

    Find Duplicate Subtrees. Incomplete

---
+++ FindDuplicateSubtrees/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace FindDuplicateSubtrees
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+        {
+            int counter = 0;
+
+            while (root.right != null || root.left != null)
+            {
+                counter++;
+            }
+
+            if (counter == 0 || counter == 1)
+            {
+                return new List<TreeNode>();
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
