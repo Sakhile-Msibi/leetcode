2a58f4d6d9eb68d89dd4b7b00d5f8c2f312c7fcf
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2023-12-01 07:11:11 +0200

    Design Circular Queue, Incomplete

--- DesignCircularQueue/Program.cs
+++ DesignCircularQueue/Program.cs
@@ -1,7 +1,43 @@
 ﻿namespace DesignCircularQueue
 {
-    internal class Program
+    internal class MyCircularQueue
     {
+        public MyCircularQueue(int k)
+        {
+            int[] arr = new int[k];
+            int head = 0, tail = 0;
+        }
+
+        public bool EnQueue(int value)
+        {
+
+        }
+
+        public bool DeQueue()
+        {
+
+        }
+
+        public int Front()
+        {
+
+        }
+
+        public int Rear()
+        {
+
+        }
+
+        public bool IsEmpty()
+        {
+
+        }
+
+        public bool IsFull()
+        {
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
