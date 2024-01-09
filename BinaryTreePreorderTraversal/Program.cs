e021c5e7127c00fa122d035bd138e190be22584d
Sakhile-Msibi <thembinkosimsibi198@gmail.com>
2024-01-09 06:57:00 +0200

    Binary Tree Preorder Traversal

--- BinaryTreePreorderTraversal/Program.cs
+++ BinaryTreePreorderTraversal/Program.cs
@@ -29,8 +29,6 @@
                 list.Add(root.val);
             }
 
-            //List<int> list = new List<int>();
-
             if (root.left != null)
             {
                 list.Add(root.left.val);
@@ -77,20 +75,20 @@
         {
             Program program = new Program();
 
-            //TreeNode root = new TreeNode(1);
-            //root.left = null;
-            //root.right = new TreeNode(2);
-            //root.right.left = new TreeNode(3);
+            TreeNode root = new TreeNode(1);
+            root.left = null;
+            root.right = new TreeNode(2);
+            root.right.left = new TreeNode(3);
 
-            TreeNode root = new TreeNode(6);
-            root.left = new TreeNode(2);
-            root.left.left = new TreeNode(1);
-            root.left.right = new TreeNode(4);
-            root.left.right.left = new TreeNode(3);
-            root.left.right.right = new TreeNode(5);
-            root.right = new TreeNode(7);
-            root.right.right = new TreeNode(9);
-            root.right.left = new TreeNode(8);
+            //TreeNode root = new TreeNode(6);
+            //root.left = new TreeNode(2);
+            //root.left.left = new TreeNode(1);
+            //root.left.right = new TreeNode(4);
+            //root.left.right.left = new TreeNode(3);
+            //root.left.right.right = new TreeNode(5);
+            //root.right = new TreeNode(7);
+            //root.right.right = new TreeNode(9);
+            //root.right.left = new TreeNode(8);
 
             //program.DisplayTree(root);
 
